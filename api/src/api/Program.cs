76c47d71ab3ef03e13687e1875366c9fb088447b
T.D.Schreur <dennis@tdschreur.nl>
2023-05-24 15:24:05 +0200

    Fix api

--- api/src/api/Program.cs
+++ api/src/api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
@@ -8,7 +12,31 @@ public static class Program
     public static async Task Main()
     {
         IHost host = new HostBuilder()
-                    .ConfigureFunctionsWorkerDefaults()
+                    .ConfigureFunctionsWorkerDefaults(builder =>
+                    {
+                        builder.UseNewtonsoftJson()
+                              .AddApplicationInsights()
+                              .AddApplicationInsightsLogger();
+                    })
+                    .ConfigureAppConfiguration((context, configurationBuilder) =>
+                    {
+                        string rootPath = context.HostingEnvironment.ContentRootPath;
+                        string environmentName = context.HostingEnvironment.EnvironmentName;
+
+                        configurationBuilder
+                           .SetBasePath(rootPath)
+                           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                           .AddEnvironmentVariables();
+
+                        if (environmentName == "Development")
+                        {
+                            configurationBuilder.AddUserSecrets<GetWeather>();
+                        }
+                    })
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddHttpClient<WeatherApiClient>();
+                    })
                     .Build();
 
         await host.RunAsync();
---
+++ api/src/api/WeatherApiClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace api;
+
+public class WeatherApiClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
+
+    public WeatherApiClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+        _configuration = configuration;
+    }
+
+    public async Task<string> GetWeather(string coordinates)
+    {
+        string key = _configuration["MapsKey"];
+        HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, $"https://atlas.microsoft.com/weather/currentConditions/json?query={coordinates}");
+        httpRequestMessage.Headers.Add("api-version", "1.0");
+        httpRequestMessage.Headers.Add("subscription-key", key);
+
+        HttpResponseMessage response = await _httpClient.SendAsync(httpRequestMessage);
+        string content = await response.Content.ReadAsStringAsync();
+
+        return content;
+    }
+}
