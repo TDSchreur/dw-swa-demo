ba0201f1b47bf7a29e4085dfdc16b185eca4c521
T.D.Schreur <dennis@tdschreur.nl>
2023-05-30 12:06:35 +0200

    update api

--- api/src/api/GetWeather.cs
+++ api/src/api/GetWeather.cs
@@ -29,7 +29,7 @@ public class GetWeather
                              typeof(string),
                              Summary = "The response",
                              Description = "This returns the response")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
         NameValueCollection query = HttpUtility.ParseQueryString(req.Url.Query);
         string coordinates = query["coordinates"];
