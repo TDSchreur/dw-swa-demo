fdddb135e549ba76d85cb2ff3b4e296783d60e52
Dennis Schreur <dennis@tdschreur.nl>
2023-06-01 09:51:25 +0200

    Cleanup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,9 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layout';
+
 import Home from './pages/home';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
